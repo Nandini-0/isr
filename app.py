8beb24685894157ab5b069a98419f8def690c244
Nandini-0 <97585142+Nandini-0@users.noreply.github.com>
2022-03-28 18:49:52 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st 
+from PIL import Image
+import numpy as np
+from ISR.models import RRDN
+from ISR.predict import Predictor
+import os
+
+st.title("Super Resolution GAN")
+st.text("--"*50)
+st.subheader("Upload an image which you want to upscale")   
+st.spinner("Testing spinner")
+
+uploaded_file = st.file_uploader("Choose an image...", type=("jpg", "png", "jpeg"))
+if uploaded_file is not None:
+    file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type}
+    st.write(file_details)
+    img = Image.open(uploaded_file)
+    st.image(img)
+    with open(os.path.join("input",uploaded_file.name),"wb") as f: 
+      f.write(uploaded_file.getbuffer())         
+    #st.success("Saved File")
+
+
+    st.image(img, caption='Uploaded Image.')
+    st.write("")
+    if st.button('Upscale Now'):
+        st.write("upscaling...")
+        lr_img = np.array(img)
+        model = RRDN(weights='gans') 
+        predictor = Predictor(input_dir='input',output_dir='output')
+        predictor.get_predictions(model=model, weights_path='weights/rdn-C6-D20-G64-G064-x2_ArtefactCancelling_epoch219.hdf5')
+        #sr_img = model.predict(np.array(lr_img))
+        #pred = Image.fromarray(pred)
+        #st.image(pred, caption='Upscaled Image', use_column_width=True)  
