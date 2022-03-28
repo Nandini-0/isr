59bee6c67fd9527997ba04cb7625fa65ccaa4e77
Nandini-0 <97585142+Nandini-0@users.noreply.github.com>
2022-03-28 15:45:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.text("--"*50)
 def predict(img):
         lr_img = np.array(img)
         #model = RRDN(weights='gans')
-        model = RDN(weights='psnr-large')
+        model = RDN(weights='psnr-small')
         sr_img = model.predict(np.array(lr_img))
         return (Image.fromarray(sr_img))
 
